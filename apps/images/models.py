ebdfb55dcd052e154ba6b62b46135a7b07b0a734
niracler <1026037967@qq.com>
2019-04-26 11:34:55 +0800

    基本完善

--- apps/images/models.py
+++ apps/images/models.py
@@ -5,8 +5,18 @@ from datetime import datetime
 # Create your models here.
 
 class Images(models.Model):
+
+    CATEGORY_TYPE = (
+        (100, "班级头像"),
+        (101, "班级logo"),
+        (200, "活动"),
+    )
+
     image = models.ImageField(upload_to="", verbose_name="图片")
     add_time = models.DateField(default=datetime.now, verbose_name="添加时间 ")
+    class_id = models.CharField(default="0", max_length=30, verbose_name="班级ID")
+    activity_id = models.CharField(default="0", max_length=30, verbose_name="活动ID")
+    category = models.IntegerField(choices=CATEGORY_TYPE, default=200, verbose_name="类型")
 
     class Meta:
         verbose_name = "图片"
@@ -14,3 +24,11 @@ class Images(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+# class Class(models.Model):
+#     class Meta:
+#         verbose_name = "班级"
+#         verbose_name_plural = verbose_name
+#
+#     def __str__(self):
--- apps/images/serializer.py
+++ apps/images/serializer.py
@@ -6,3 +6,14 @@ class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Images
         fields = "__all__"
+
+
+class ImagesCreateSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=True, label="图片")
+    class_id = serializers.CharField(required=True, label="班级ID")
+    activity_id = serializers.CharField(required=True, label="活动ID")
+    category = serializers.IntegerField(required=True, label="类型")
+
+    class Meta:
+        model = Images
+        fields = ('image', 'class_id', 'activity_id', 'category')
